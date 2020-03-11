5461837dd436531cdec08f3d038bd57bd5cb3e0c
Xiao Jin <debugeek@gmail.com>
2020-03-11 18:31:20 +0800

    version comparing bug fix

--- centos-setup.sh
+++ centos-setup.sh
@@ -1,24 +1,44 @@
-
 #!/bin/bash
 
-yum update -y
-yum upgrade -y
+set -e
 
-yum install epel-release -y
-yum install yum-utils -y
-
-# Firewall
-
-systemctl start firewalld
-
+version_compare () {
+    if [[ $1 == $2 ]]
+    then
+        return 0
+    fi
+    local IFS=.
+    local i ver1=($1) ver2=($2)
+    # fill empty fields in ver1 with zeros
+    for ((i=${#ver1[@]}; i<${#ver2[@]}; i++))
+    do
+        ver1[i]=0
+    done
+    for ((i=0; i<${#ver1[@]}; i++))
+    do
+        if [[ -z ${ver2[i]} ]]
+        then
+            ver2[i]=0
+        fi
+        if ((10#${ver1[i]} > 10#${ver2[i]}))
+        then
+            return 1
+        fi
+        if ((10#${ver1[i]} < 10#${ver2[i]}))
+        then
+            return 2
+        fi
+    done
+    return 0
+}
 
 #
 # fail2ban
 #
+installFail2ban() {
+    yum install -y fail2ban
 
-yum install -y fail2ban
-
-cat > /etc/fail2ban/jail.local <<EOF
+    cat > /etc/fail2ban/jail.local <<EOF
 [ssh-iptables]
 enabled = true
 filter = sshd
@@ -27,16 +47,15 @@ logpath = /var/log/secure
 maxretry = 5
 EOF
 
-systemctl restart fail2ban.service
-systemctl enable fail2ban
-
+    systemctl restart fail2ban.service
+    systemctl enable fail2ban
+}
 
 #
 # BBR
 #
-
 installBBR() {
-    if [[ $(uname -r) < "4.9" ]] ;then
+    if [[ $(version_compare $(uname -r) "4.9") == 2 ]] ;then
         rpm --import https://www.elrepo.org/RPM-GPG-KEY-elrepo.org
         rpm -Uvh http://www.elrepo.org/elrepo-release-7.0-2.el7.elrepo.noarch.rpm
         yum-config-manager --enable elrepo-kernel
@@ -59,20 +78,8 @@ EOF
 }
 
 #
-# Docker
-#
-
-installDocker() {
-    curl -fsSL https://get.docker.com/ | sh
-    systemctl start docker
-    systemctl status docker
-    systemctl enable docker
-}
-
-#
 # V2Ray
 #
-
 installV2Ray() {
     curl -fsSL https://install.direct/go.sh | sh
 
@@ -175,14 +182,24 @@ EOF
 }
 
 
+
+yum update -y
+yum upgrade -y
+
+yum install epel-release -y
+yum install yum-utils -y
+
+# Firewall
+systemctl start firewalld
+
+read -p "Shall I Install Fail2ban (y/n)? " fail2ban
+if [ "$fail2ban" != "${fail2ban#[Yy]}" ] ;then
+    installFail2ban
+fi
+
 read -p "Shall I Install BBR (y/n)? " bbr
 if [ "$bbr" != "${bbr#[Yy]}" ] ;then
     installBBR
-fi
-
-read -p "Shall I Install Docker (y/n)? " docker
-if [ "$docker" != "${docker#[Yy]}" ] ;then
-    installDocker
 fi
 
 read -p "Shall I Install V2Ray (y/n)? " v2ray
